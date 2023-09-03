efa9adc3d5e6c2f6e81a0cc0cb548fc34ac2a997
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-03 03:44:21 +0000

    tthrow away

--- src/examples/01_data_types/01_int/int.h
+++ src/examples/01_data_types/01_int/int.h
@@ -1,4 +1,3 @@
-/*
-Function echo_variable
-*/
+// Create prototype for void function scho_variable with one int parameter
+// Header is the ouline of Function, signature, prototype, header
 int echo_variable(int num);
