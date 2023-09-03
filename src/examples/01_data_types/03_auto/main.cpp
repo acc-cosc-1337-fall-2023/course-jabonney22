4e7ba155a2ce5259d4445fbbaf596c33a277c5fa
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-03 04:42:14 +0000

    Intro to auto keyword

---
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using std:: cout;
+
+int main()
+
+{
+   auto num = 5;
+
+   cout<<"auto value is:"<<num<<"\n";
+
+   num = num + 10;
+
+   cout<<"auto value is:"<<num<<"\n";
+
+   auto num1 = 5.5;
+
+   cout<<"auto value is:"<<num1<<"\n";
+
+   num1 = num1 + 10.25;
+
+   cout<<"auto value is:"<<num<<"\n";
+
+   auto letter = 'a';
+
+   cout<<"letter is:"<<letter<<"\n";
+
+
+    return 0;
+}
