f6f9324ee87482ebdece70174f7e9598f2050190
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-03 04:33:58 +0000

    intro to decimals main program

---
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -0,0 +1,18 @@
+#include "decimals.h"
+#include<iostream>
+
+using std::cout; using std::cin;
+
+int main(){
+
+    double value;
+
+    cout<<"Enter a value:";
+    cin>>value;
+
+    double result = add_to_double_1(value);
+
+    cout<<"Result is:"<<result<<"\n";
+
+    return 0;
+}
