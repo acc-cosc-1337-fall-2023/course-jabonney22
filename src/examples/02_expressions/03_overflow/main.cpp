cad437c301badada6210a6fbf5088bbc5fa736c0
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-08 22:48:09 +0000

    overflow underflow int

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -1,6 +1,23 @@
+#include<iostream>
+#include<climits>
+
+using std::cout;
 
 int main()
 {
+    auto max_int = INT_MAX;
+    cout<<"Max int: "<<max_int<<"\n";
+
+    max_int = max_int + 1;
+    cout<<"Max int: "<<max_int<<"\n";
+
+    auto min_int = INT_MIN;
+    cout<<"Min int: "<<min_int<<"\n";
+
+    min_int = min_int -1;
+    cout<<"Min int: "<<min_int<<"\n";
 
     return 0;
+
+
 }
