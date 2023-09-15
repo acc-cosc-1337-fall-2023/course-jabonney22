fcaa0af3ad2ea7aae2a1275d1a7da22c7e1d9191
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-15 00:37:59 +0000

    while

--- src/examples/04_repetition/01_while/main.cpp
+++ src/examples/04_repetition/01_while/main.cpp
@@ -1,7 +1,17 @@
 #include "while.h"
 
+using td
+
 int main() 
 {
+
+	string str = "Hello";
+
+	display_string(str);
+
+	auto num = 5;
+	auto result = sum_of_squars(num);
+	cout<<"Result is: "<<result<<"\n";
 
 	return 0;
 }
--- src/examples/04_repetition/01_while/while.cpp
+++ src/examples/04_repetition/01_while/while.cpp
@@ -3,7 +3,18 @@
 //the sum of all the squares of that number from 1 to the number. 
 //Example: sum_of_squares(4) 
 //1*1 + 2 * 2 + 3*3 + 4*4 = 30
+int sum_of_squares(int num){
 
+    auto sum = 0;
+
+    while(num > 0){
+
+        sum = sum + num * num;
+        num--;
+
+}
+    return sum;
+}
 
 //Using a while loop write code for display function that accepts a string and displays 
 //each character of the string in a line
@@ -18,4 +29,13 @@ l
 0
 
 */
+void display_string(std::string str){
 
+    auto indx = 0;
+
+    while(indx < str.size())
+    {
+        cout<<str[indx]<<"\n";
+        indx++;
+    }
+}
