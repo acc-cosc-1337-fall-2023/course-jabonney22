6efee320a0d1e40b00a2748f6ada1b993e6717a5
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-15 00:00:56 +0000

    Use Menu

--- src/examples/03_conditionals/04_switch/main.cpp
+++ src/examples/03_conditionals/04_switch/main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#inlcude<switch.h>
+
+using std:: cout; using std:: cin
 
 int main() 
 {
+
+	auto option = 0;
+	cout<<"Enter a number: ";
+	cin>>option;
 	
+	string result = menu(option);
+	cout<<result<<"\n"
 
 	return 0;
 }
