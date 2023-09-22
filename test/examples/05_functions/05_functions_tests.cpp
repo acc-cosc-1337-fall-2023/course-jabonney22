38395d7347d12316c5e38ac6c720dc93f34969a4
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-22 00:47:42 +0000

    functions overload

--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -3,6 +3,7 @@
 #include "void.h"
 #include "val_ref.h"
 #include "static.h"
+#include "default.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -51,4 +52,19 @@ TEST_CASE("test static_2 function", "value of num persists across function calls
 	REQUIRE(static_var_2() == 8);
 	
 	
+}
+
+TEST_CASE("test static_2 function", "value of num persists across function calls") {
+	
+	REQUIRE(static_var_2() == 6);
+	REQUIRE(static_var_2() == 7);
+	REQUIRE(static_var_2() == 8);
+	
+	
+}
+
+TEST_CASE("Test function overloads")
+{
+	REQUIRE(get_weekly_pay(52000) == 1000);
+	REQUIRE(get_weekly_pay(40, 25) == 1000);
 }
