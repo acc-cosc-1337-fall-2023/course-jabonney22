8685920f3e6672cdcb134190a86411e1e1d1773a
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-23 16:36:04 +0000

    Homework 4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,60 @@
 //write include statements
+#include<iostream>
+#include"repetition.h"
 
 //write using statements
 
-int main() 
-{
+using std::cin; using std::cout; using std::string; 
+
+int main() {
+
+    int option;
+    int number;
+    int number1;
+    int number2;
+    int result;
+
+do {
+
+    cout<<"Option"<<"\n";
+	cout<<"1-Factorial"<<"\n";
+    cout<<"2-Greatest Common Divisor"<<"\n";
+	cout<<"3-Exit" <<"\n";
+	cout<<"Enter a value between 1-3: "<<"\n";
+	cin>>option;
+
+    if (option == 1){
+
+        cout<<"Enter a number: ";
+        cin>>number;
+
+        result = factorial(number);
+        cout<<"Factorial of "<<number<<" is: "<<result<<"\n";
+    
+    }
+
+    else if(option == 2){
+
+        cout<<"Enter the first number: ";
+        cin>>number1;
+
+        cout<<"Enter the second number: ";
+        cin>>number2;
+
+        result = gcd(number1 , number2);
+        cout<<"Greatest common divisor of "<<number1<<"and"<<number2<<" is: "<<result<<"\n";
+    
+    }
+
+    break;
+
+} while (option == 1 || 2 || 3);
+
 	return 0;
 }
+
+
+
+
+
+	
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,34 @@
 //add include statements
+#include "repetition.h"
 
 //add function(s) code here
+
+int factorial(int num){
+
+    auto sum = 1;
+
+    while(num > 0){
+
+        sum *= num;
+        num--;
+
+    }
+    return sum;
+}
+
+int gcd(int num1, int num2){
+
+    while(num1 != num2){
+
+        if (num1 < num2){
+
+            std:: swap(num1 , num2);
+        }
+
+        else if (num1 > num2){
+
+            num1-=num2;
+        }
+    }
+    return num1;
+}
