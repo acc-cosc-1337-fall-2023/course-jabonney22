86418073b7d1bf4039055f406f77183e19df16b0
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-09-29 00:33:13 +0000

    vector creation

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -8,35 +8,29 @@ int main()
 {
 
     vector<int> nums{7, 4, 20};
+    vector<int> nums1;
+    vector<int> nums2(5,10);
 
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
+    for(auto num2 : nums2){
 
-    for(auto i=0; i < nums.size(); i++)
-    {
-        cout<<nums[i]<<"\n";
+        cout<<num2<<"\n";
     }
     cout<<"\n";
-    nums.push_back(3);
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
 
-    for(auto num: nums)
-    {
-        cout<<num<<"\n";
+    vector<int> nums3 = nums;
+
+    for(auto num3: nums3){
+
+        cout<<num3<<"\n";
     }
-    cout<<"\n";
-    nums.push_back(15);
-    nums.push_back(25);
-
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
-
-    nums.push_back(0);
-
-    cout<<"\n";
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
     
+    vector<double> doubles{5.5, 3.9, 8.99};
+    cout<<"\n";
+    
+    for(auto dub: doubles){
+
+        cout<<dub<<"n";
+    }
+
     return 0;
 }
