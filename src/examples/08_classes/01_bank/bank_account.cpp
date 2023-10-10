dae1d312fc85d327ca7f2fa638e1754c14193f5b
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-10-10 03:02:02 +0000

    class account deposit and withdraw

--- src/examples/08_classes/01_bank/bank_account.cpp
+++ src/examples/08_classes/01_bank/bank_account.cpp
@@ -1,9 +1,27 @@
 //bank_account.cpp
 #include "bank_account.h"
 
-using std :: cout;
+void BankAccount::deposit(int amount)
+{
+    if(amount > 0)//the class sets the rules for modifying its data
+    {
+        balance += amount;
+    }
+}
 
-void say_hello(){
+void BankAccount::withdraw(int amount)
+{
+    if(amount > 0 && amount <= balance)
+    {
+        balance -= amount;
+    }
+}
 
-    cout<<"Hello World!\n";
+
+
+//NOT PART OF THE BANK ACCOUNT CLASS
+//FREE FUNCTION
+void say_hello()
+{
+    std::cout<<"Hello World!\n";
 }
--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -3,15 +3,16 @@
 
 class BankAccount{
 
-public:
+public://access specifier-other code can access these functions
     // constructor
+    BankAccount(){}// no parameters=default constructor
     BankAccount(int b) : balance(b){/*empty class function code*/}
-    int get_balance(){return balance;}
+    void deposit(int ammount);
+    void withdraw(int amount);
+    int get_balance() const {return balance;}
 
-private:
+private:// access specifier-onlly this class can access function or varaibles that are private
 
     int balance{0}; //intialize to zero; happens first
 
 };
-
-void say_hello();
