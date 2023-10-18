c0b8c4028950a7c58eb45206bf7bbb076a90ca89
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-10-18 00:34:28 +0000

    Inheritance account savings and checking

--- src/examples/08_classes/01_bank/savings_account.h
+++ src/examples/08_classes/01_bank/savings_account.h
@@ -1,1 +1,17 @@
 //savings_account.h
+#include "bank_account.h"
+
+#ifndef SAVINGS_ACCCOUNT_H
+#define SAVINGS_ACCCOUNT_H
+
+class SavingsAccount: public BankAccount{
+
+public:
+
+    SavingsAccount(){}
+    SavingsAccount(int b) : BankAccount(b) {/**EMPTY CODE BLOCK*/}
+
+private:
+};
+
+#endif
