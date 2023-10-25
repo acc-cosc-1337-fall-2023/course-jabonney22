bd370e89c427f5767d4238bb7d02aeacbe01dbe5
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-10-25 01:49:06 +0000

    ref and pointers function parameters

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,32 +1,23 @@
 //bank_account.h
 #include<iostream>
 #include<stdlib.h>//access rand() function
-
 //class header guards
 #ifndef BANK_ACCOUNT_H
 #define BANK_ACCOUNT_H
-
-class BankAccount{
-
-friend void show_balance(BankAccount& account);
-
+class BankAccount
+{
+friend void show_balance(const BankAccount& account);
 public://access specifier-other code can access these functions
-    // constructor
-    BankAccount(){get_balance_from_db();}// no parameters=default constructor
-    BankAccount(int b) : balance(b){/*empty class function code*/}
-    void deposit(int ammount);
+    //constructor
+    BankAccount(){get_balance_from_db();}//no parameters=default constructor
+    BankAccount(int b) : balance(b){/*empty class function code */}
+    void deposit(int amount);
     void withdraw(int amount);
-    int get_balance() const {return balance;}
+    virtual int get_balance() const {return balance;}
 
-private:// access specifier-onlly this class can access function or varaibles that are private
-
-    int balance{0}; //intialize to zero; happens first
+private://access specifier-only this class can access function or variables that are private
+    int balance{0};//initialize to zero; happens first
     void get_balance_from_db();
 };
-
-
-
 void say_hello();
-
 #endif
-
--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,71 +1,80 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include"bank_account.h"
-#include "savings_account.h"
 #include "checking_account.h"
-
+#include "savings_account.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
 TEST_CASE("Test bank account get balance")
 {
-	CheckingAccount account(500);
-	REQUIRE(account.get_balance() == 500);
+	BankAccount* account;
+	CheckingAccount checking_account(500);
+	account = & checking_account;
+
+	REQUIRE(account->get_balance() == 500);
 }
 
 TEST_CASE("Test bank deposit w 0 balance")
 {
-	SavingsAccount account(0);
-	REQUIRE(account.get_balance() == 0);
+	BankAccount* account;
+	CheckingAccount checking_account(0);
+	account = &checking_account;
 
-	account.deposit(150);
-	REQUIRE(account.get_balance() == 150);
+	REQUIRE(account->get_balance() == 0);
+
+	account->deposit(150);
+	REQUIRE(account->get_balance() == 150);
 }
 
 TEST_CASE("Test account deposit function")
 {
-	CheckingAccount account1(500);
-	CheckingAccount account2(1000);
+	BankAccount* account;
+	CheckingAccount checking_account(500);
+	account = &checking_account;
 
-	REQUIRE(account1.get_balance() == 500);
-	REQUIRE(account2.get_balance() == 1000);
+	REQUIRE(account->get_balance() == 500);
 
-	account1.deposit(50);
-	REQUIRE(account1.get_balance() == 550);
+	account->deposit(50);
+	REQUIRE(account->get_balance() == 550);
 
-	account2.deposit(100);
-	REQUIRE(account2.get_balance() == 1100);
 }
 
 TEST_CASE("Test account withdraw w 0 balance")
 {
-	SavingsAccount account(0);
-	REQUIRE(account.get_balance() == 0);
+	BankAccount* account;
+	SavingsAccount savings_account(0);
+	account = &savings_account;
 
-	account.withdraw(100);
-	REQUIRE(account.get_balance() == 0);
+	REQUIRE(account->get_balance() == 5);
+
+	account->withdraw(100);
+	REQUIRE(account->get_balance() == 5);
 }
 
 TEST_CASE("Test account withdraw")
 {
-	SavingsAccount account(1000);
-	REQUIRE(account.get_balance() == 1000);
+	BankAccount* account;
+	SavingsAccount savings_account(1000);
+	account = &savings_account;
 
-	account.withdraw(100);
-	REQUIRE(account.get_balance() == 900);
+	REQUIRE(account->get_balance() == 1005);
+
+	account->withdraw(100);
+	REQUIRE(account->get_balance() == 905);
 }
 
 TEST_CASE("Test account deposit and withdraw")
 {
-	SavingsAccount account(1000);
-	REQUIRE(account.get_balance() == 1000);
+	BankAccount* account;
+	SavingsAccount savings_account(1000);
+	account = &savings_account;
 
-	account.deposit(100);
-	REQUIRE(account.get_balance() == 1100);
+	REQUIRE(account->get_balance() == 1005);
 
-	account.withdraw(100);
-	REQUIRE(account.get_balance() == 1000);
+	account->deposit(100);
+	REQUIRE(account->get_balance() == 1105);
+
+	account->withdraw(100);
+	REQUIRE(account->get_balance() == 1005);
 }
-
-
