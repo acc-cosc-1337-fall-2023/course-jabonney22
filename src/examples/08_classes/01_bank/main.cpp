e25776194f07648075af529c0c3a90ae839311a7
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-10-31 02:37:16 +0000

    vector of bank account pointers

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,26 +1,37 @@
-#include "atm.h"
-#include "bank_account.h"
 #include "checking_account.h"
 #include "savings_account.h"
 #include<time.h>//helps provide a true random number with rand() function
-using std::cout;
+#include<vector>
+
+using std::cout; using std::vector;
+
 int main()
 {
+
 	srand(time(NULL));
 	BankAccount* account = nullptr;//create a pointers; points to nothing NULL
 
-	/*CheckingAccount checking_account(500);
+	CheckingAccount checking_account(500);
 	account = &checking_account;
-	cout<<account->get_balance()<<"\n";*/
+	cout<<account->get_balance()<<"\n";
 
 	SavingsAccount savings_account(500);
 	account = &savings_account;
 	cout<<account->get_balance()<<"\n";
 
+	vector<BankAccount*> accounts;
+	accounts.push_back(&checking_account);
+	accounts.push_back(&savings_account);
+
+	for(auto account: accounts)
+	{
+		cout<<"Balance: "<<account->get_balance()<<"\n";
+	}
+
+	/*
 	ATM atm(account);
 	run_menu(atm);
-
-	cout<<"Balance: "<<account->get_balance()<<"\n";
+	cout<<"Balance: "<<account->get_balance()<<"\n";*/
 
 	return 0;
 }
