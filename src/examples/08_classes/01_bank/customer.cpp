5d331e10711c593c97b4f9e0c1845016b414403c
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-10-31 02:42:54 +0000

    add customer class to atm example

--- src/examples/08_classes/01_bank/customer.cpp
+++ src/examples/08_classes/01_bank/customer.cpp
@@ -1,1 +1,12 @@
 //customer.cpp
+
+#include "customer.h"
+
+Customer::Customer()
+{
+    CheckingAccount checking_account;
+    accounts.push_back(&checking_account);
+
+    SavingsAccount savings_account;
+    accounts.push_back(&savings_account);
+}
