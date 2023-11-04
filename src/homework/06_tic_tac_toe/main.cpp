44cc639e1819f622dd167399126524c094ac6b79
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-11-04 20:07:32 +0000

    Homework 6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -9,53 +9,33 @@ int main()
 {
 	TicTacToe game;
 	string first_player;
-	char user_choice;
+	char user_choice = 'y';
 
 	do{
-
-		cout<<"Enter first player (X/O): ";
+		cout<<"Enter first player";
 		cin>>first_player;
-
 		game.start_game(first_player);
 
 		int position;
 
 		while(!game.game_over()){
 
-			cout<<"Enter a position: ";
+			cout<<"Enter a positon: ";
 			cin>>position;
 			game.mark_board(position);
 			game.display_board();
 
 		}
 
-		game.display_board();
-    	std::string winner = game.get_winner();
-
-    	if (winner == "C") {
-        cout << "It's a tie!" <<"\n";
-
-		game.game_over();
-   		 } 
-		 
-		else {
-
-        cout << winner << " wins!" <<"\n";
-		game.game_over();
-   		 }
+		cout<<"Play again, enter y or Y?: ";
+		cin>>user_choice;
 
 
-        cout << "Play again? (y/n): ";
-        cin >> user_choice;
 
-    return 0;
-
-		
-	}while(user_choice == 'y' || user_choice == 'Y');
+	}while(user_choice == 'y' || user_choice == 'Y' );
 
 	return 0;
 }
-
 
 
 
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,37 +1,18 @@
 //cpp
 #include "tic_tac_toe.h"
-#include<iostream>
-#include<vector>
-#include<string>
 
-using std::cout; 
-
+using std::cout;
 
 bool TicTacToe::game_over(){
-    
-    if (check_column_win() == true || check_row_win() ==  true || check_diagonal_win() == true) {
-            return true;
-    } 
-    
-    else if (check_board_full() == true && check_column_win() == false && check_row_win() ==  false && check_diagonal_win() == false) {
-            winner = "C";
-            return true;
-    }
 
-    else{
-
-        if(get_winner() != "X" || get_winner() != "O" || get_winner() != "C" || !check_board_full()){
-
-        return false;
-
-        }
-    }
+    return check_board_full();
 }
 
-void TicTacToe::start_game(std::string first_player){
+void TicTacToe::start_game(std:: string first_player){
 
     player = first_player;
     clear_board();
+
 }
 
 void TicTacToe::mark_board(int position){
@@ -43,24 +24,22 @@ void TicTacToe::mark_board(int position){
 
 void TicTacToe::display_board() const{
 
-    for( long unsigned int i = 0; i < pegs.size(); i += 3 ){
+    for(long unsigned int i=0; i < pegs.size(); i += 3){
 
-    cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
+        cout<<pegs[i]<<"|"<<pegs[i + 1]<<"|"<<pegs[i + 2]<<"\n";
 
     }
 }
 
 
-//private functions
 
-std::string winner;
+//private functions
 
 void TicTacToe::clear_board(){
 
     for(auto& peg: pegs){
 
         peg = " ";
-
     }
 }
 
@@ -68,79 +47,30 @@ void TicTacToe::set_next_player(){
 
     if(player == "X"){
 
-        player = "O";
+        player = "Y";
+
     }
+
     else{
 
         player = "X";
     }
+
 }
 
 bool TicTacToe::check_board_full(){
 
-    for( const std::string& peg : pegs){
+    for(long unsigned int i=0; i < pegs.size(); i++){
 
-        if(peg == " "){
+        if(pegs[i] == " "){
+
             return false;
         }
     }
 
     return true;
+
 }
-
-bool TicTacToe::check_column_win(){
-
-    for(int i = 0 ; i < 3; i++ ){
-        if(pegs[i] == winner && pegs[i+3] == winner && pegs[i+6] == winner){
-
-            set_winner();
-        }
-    }
-
-    return true;
-}
-
-bool TicTacToe::check_diagonal_win(){
-
-    for(int i = 0 ; i < 3; i++ ){
-
-         if ((pegs[0] == winner && pegs[4] == winner && pegs[8] == winner) ||
-            (pegs[2] == winner && pegs[4] == winner && pegs[6] == winner)){
-
-            set_winner();
-
-        }
-    }
-
-    return true;
-}
-
-bool TicTacToe::check_row_win(){
-
-    for(int i = 0 ; i < 9; i+=3 ){
-        if(pegs[i] == winner && pegs[i+3] == winner && pegs[i+6] == winner){
-
-        set_winner();
-
-        }
-    }
-
-    return true;
-}
-
-void TicTacToe::set_winner(){
-    
-    if(winner == "X"){
-
-        winner == "O";
-    }
-    else{
-
-        winner == "X";
-    }
-}
-
-
 
 
 
